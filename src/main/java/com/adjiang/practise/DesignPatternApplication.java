53fbc9775aa14291f7e83e484fc2a132a55d9d61
DaoDone <l>
2021-09-26 00:08:36 +0800

    day01

--- src/main/java/com/adjiang/practise/DesignPatternApplication.java
+++ src/main/java/com/adjiang/practise/DesignPatternApplication.java
@@ -1,4 +1,4 @@
-package com.adjiang.practise.designpattern;
+package com.adjiang.practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/adjiang/practise/DesignPatternApplicationTests.java
+++ src/test/java/com/adjiang/practise/DesignPatternApplicationTests.java
@@ -1,4 +1,4 @@
-package com.adjiang.practise.designpattern;
+package com.adjiang.practise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
